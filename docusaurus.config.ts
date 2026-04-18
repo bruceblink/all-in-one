f7b2dbf64cd07ea03fa318a36a1186fae773f8bf
likanug <likanug.g@qq.com>
2026-04-18 16:51:44 +0800

    feat: 更新 favicon 为 SVG，与 Logo 风格统一

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Likanug',
   tagline: '记录思考，沉淀知识，持续构建。',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.svg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
